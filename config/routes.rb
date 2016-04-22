dd3820b579f13fb5c01ae04a7278fd89b90e11ae
Rose Lu <roseyylu@gmail.com>
2016-04-23 01:33:32 +1200

    make page for merrin

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "hi_tim", to: "friend_pages#hi_tim"
   get "hi_jared", to: "friend_pages#hi_jared"
   get "hi_matthew", to: "friend_pages#hi_matthew"
+  get "hi_merrin", to: "friend_pages#hi_merrin"
 end
