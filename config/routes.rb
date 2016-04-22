f9a2f2a42afbae5d3f93e094343cd652c19845c4
Rose Lu <roseyylu@gmail.com>
2016-04-23 00:48:57 +1200

    make page for jared

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "hi_tim", to: "pages#hi_tim"
   get "hi_tim", to: "friend_pages#hi_tim"
+  get "hi_jared", to: "friend_pages#hi_jared"
 end
