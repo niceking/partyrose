89ff50c7aa5efa761ad39a5c2439495811b1372a
Rose Lu <roseyylu@gmail.com>
2016-04-23 15:10:20 +1200

    make it tessellate all the way

--- app/assets/javascripts/alternate.js
+++ app/assets/javascripts/alternate.js
@@ -4,13 +4,10 @@ jQuery(document).ready(function() {
   var addRows = function() {
     $(".alternate").empty();
     height = $(window).height();
-    while(height >= 40) {
+    while(height >= 0) {
       $(".alternate").append("<div class='partyrose repeat-x'> </div>");
       $(".alternate").append("<div class='partyrose-reverse repeat-x'> </div>");
       height = height - 40;
-    }
-    if(height > 20) {
-      $(".alternate").append("<div class='partyrose repeat-x'> </div>");
     }
   };
 
