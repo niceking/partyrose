98c08822f7b965ab3d95f8f1bd03d3910d84c1b5
Rose Lu <roseyylu@gmail.com>
2016-08-07 18:51:51 +0800

    update config for heroku

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Partyrose
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+
+    # Must be false for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
